33cb45608878956eb152399b6bd4297bb50f8b55
Adylsha Yumayev <adylsha.yumayev@gelecekvarlik.com.tr>
2025-06-20 15:51:54 +0300

    theme customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { Input } from '@/components/ui/input';
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Progress } from '@/components/ui/progress';
+
 export default function Home() {
-    return (
-        <div>
-            <p className="text-rose-500">
-                Hello World!
-            </p>
-        </div>
-    );
+  return (
+    <div className='p-4'>
+      <div className='flex flex-col gap-y-4'>
+        <Input />
+        <Button variant={"elevated"}>
+          I am a button
+        </Button>
+        <Textarea />
+        <Checkbox />
+        <Progress value={50} />
+      </div>
+    </div>
+  );
 }
