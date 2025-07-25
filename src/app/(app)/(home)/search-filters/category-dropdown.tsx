b3a922cb09ef2e1acc2d2dc25ca1b2f3241b6d47
Adylsha Yumayev <adylsha.yumayev@gelecekvarlik.com.tr>
2025-07-25 09:43:52 +0300

    Refactor category management with TRPC integration, update environment variables, and enhance sidebar functionality

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { useRef, useState } from 'react';
+import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useDropdownPosition } from './use-dropdown-position';
 import { SubcategoryMenu } from './subcategory-menu';
-import { CustomCategory } from '../types';
-import Link from 'next/link';
+import { CategoriesGetManyOutput } from '@/modules/categories/types';
 
 interface Props {
-  category: CustomCategory;
+  category: CategoriesGetManyOutput[1];
   isActive?: boolean;
   isNavigationHovered?: boolean;
 }
